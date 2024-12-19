8c48e11557b3f8d71f26c2ed6a13d47e715e8b37
Abhishek khare <ghostabhi323@gmail.com>
2024-12-19 08:19:18 +0530

    fix : changed some project files

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import Navbar from './components/navbar/Navbar'
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
+import LoginModal from './components/modals/LoginModal'
+import getCurrentUser from './actions/getCurrentUser'
+import RentModal from './components/modals/RentModal'
+import SearchModal from './components/modals/SearchModal'
+
+const font = Nunito({ subsets: ['latin'] })
+
+export const dynamic = 'force-dynamic'
+
+export const metadata: Metadata = {
+  title: 'HorizonHomes',
+  description: 'Holiday Homes & Apartment Rentals',
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+  const currentUser = await getCurrentUser();
+
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ToasterProvider/>
+        <SearchModal/>
+        <RentModal/>
+        <LoginModal/>
+        <RegisterModal/>
+        <Navbar currentUser={currentUser}/>
+        <div className='pb-20 pt-28 '>
+          {children}
+        </div>
+      </body>
+    </html>
+  )
+}
